fd8a0c9add58e9077f40b0b4cefa93f48d497103
Iqra Arif <bintearif79@gmail.com>
2025-05-06 06:25:27 -0700

    Add files via upload

---
+++ Variable practice.py
@@ -0,0 +1,15 @@
+# This program is written in snake case
+# we cannot use spaces like a student name it's a invalid because it contains a space,and space are not allowed in variable names.   
+# so,use underscore (_), like student_name.                                
+student_name="Iqra Arif"
+father_name="M.Arif Mehdi"
+serial_number="0023"
+student_age=20
+marks=429
+address="Sargodha,Nehang,House no.36"
+print(student_name)
+print(father_name)
+print(serial_number)
+print(student_age)
+print(marks)
+print(address)
